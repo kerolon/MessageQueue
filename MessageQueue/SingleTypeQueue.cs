6df78ac5cdefa42c1c0bad10d51f570bd6d73dd0
健憲 飯嶋 <princecharlesalexander@yahoo.co.jp>
2017-03-28 13:38:02 +0900

    プロジェクト ファイルを追加します。

---
+++ MessageQueue/SingleTypeQueue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Messaging;
+
+namespace MQ
+{
+    public class SingleTypeQueue
+    {
+        public string QueueName { get; private set; }
+        public bool DeleteExistingQueueWithSending { get; private set; }
+        public bool DeleteExistingQueueWithReceiving { get; private set; }
+        public bool Transactional { get; private set; }
+        public string QueuePath { get; set; }
+        public SingleTypeQueue(string queueName)
+        {
+            QueueName = queueName;
+            QueuePath = $"{Environment.MachineName}\\private$\\{queueName}";
+        }
+
+        public SingleTypeQueue(string queueName,bool deleteExistingQueueWithSending, bool deleteExistingQueueWithReceiving, bool transactional)
+        {
+            QueueName = queueName;
+            DeleteExistingQueueWithSending = deleteExistingQueueWithSending;
+            DeleteExistingQueueWithReceiving = deleteExistingQueueWithReceiving;
+            Transactional = transactional;
+            QueuePath = $"{Environment.MachineName}\\private$\\{queueName}";
+        }
+
+
+        public void SendMessage<T>(T messageBody) where T:class
+        {
+            using (var mQueue = CreateNewMessageQueue(QueueName,DeleteExistingQueueWithSending,Transactional))
+            {
+                var message = new Message()
+                {
+                    Label = "test",
+                    Body = messageBody,
+                };
+                message.Formatter = new XmlMessageFormatter(new Type[] { typeof(T) });
+                mQueue.Send(message);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="timeout">単位は秒</param>
+        /// <returns></returns>
+        public T ReceiveMessage<T>(int timeout) where T:class
+        {
+
+            using (var mQueue = CreateNewMessageQueue(QueueName,DeleteExistingQueueWithReceiving,Transactional))
+            {
+                var message = mQueue.Receive(new System.TimeSpan(0, 0, timeout));
+                message.Formatter =new XmlMessageFormatter(new Type[] { typeof(T) });
+                return message.Body as T;
+            }
+        }
+
+
+        private MessageQueue CreateNewMessageQueue(string queueName,bool deleteExistingQueue, bool transactional)
+        {
+            return (new Func<MessageQueue>(() =>
+            {
+                if (MessageQueue.Exists(QueuePath))
+                {
+                    if (deleteExistingQueue)
+                    {
+                        MessageQueue.Delete(QueuePath);
+                        return MessageQueue.Create(QueuePath, transactional: transactional);
+                    }
+                    else
+                    {
+                        return new MessageQueue(QueuePath);
+                    }
+                }
+                else
+                {
+                    return MessageQueue.Create(QueuePath, transactional: transactional);
+                }
+            })());
+        }
+
+    }
+}
---
+++ MessageQueueTest/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQ;
+using Xunit;
+using FakeItEasy;
+using System.Messaging;
+
+namespace MQTest
+{
+    public class Class1
+    {
+
+
+        [Fact]
+        public void デフォルトコンストラクタで正しく初期化される事()
+        {
+            string name = "queue_name";
+
+            var q = new MQ.SingleTypeQueue(name);
+
+            Assert.Equal(name, q.QueueName);
+            Assert.False(q.DeleteExistingQueueWithSending);
+            Assert.False(q.DeleteExistingQueueWithReceiving);
+            Assert.False(q.Transactional);
+        }
+
+        [Fact]
+        public void コンストラクタで正しく初期化される事1()
+        {
+            string name = "queue_name1";
+
+            var q1 = new MQ.SingleTypeQueue(name,true,false,false);
+
+            Assert.Equal(name, q1.QueueName);
+            Assert.True(q1.DeleteExistingQueueWithSending);
+            Assert.False(q1.DeleteExistingQueueWithReceiving);
+            Assert.False(q1.Transactional);
+        }
+
+        [Fact]
+        public void コンストラクタで正しく初期化される事2()
+        {
+            string name = "queue_name2";
+
+            var q2 = new MQ.SingleTypeQueue(name, false, true, false);
+
+            Assert.Equal(name, q2.QueueName);
+            Assert.False(q2.DeleteExistingQueueWithSending);
+            Assert.True(q2.DeleteExistingQueueWithReceiving);
+            Assert.False(q2.Transactional);
+
+        }
+
+        [Fact]
+        public void コンストラクタで正しく初期化される事3()
+        {
+            string name = "queue_name3";
+
+            var q3 = new MQ.SingleTypeQueue(name, false, false, true);
+
+            Assert.Equal(name, q3.QueueName);
+            Assert.False(q3.DeleteExistingQueueWithSending);
+            Assert.False(q3.DeleteExistingQueueWithReceiving);
+            Assert.True(q3.Transactional);
+        }
+
+
+    }
+}
